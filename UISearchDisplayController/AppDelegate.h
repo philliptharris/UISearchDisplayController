fa81ae38de519ebaec6eb811bbaec49f956e3aab
Phillip Harris <philliptharris@gmail.com>
2014-04-19 11:42:49 -0400

    Initial Commit

---
+++ UISearchDisplayController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UISearchDisplayController
+//
+//  Created by Phillip Harris on 4/19/14.
+//  Copyright (c) 2014 Phillip Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UISearchDisplayController/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UISearchDisplayController
+//
+//  Created by Phillip Harris on 4/19/14.
+//  Copyright (c) 2014 Phillip Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
