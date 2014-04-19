012fba84cd347c36bad6fee2b6bc2a6e22e615c6
Phillip Harris <philliptharris@gmail.com>
2014-04-19 12:43:05 -0400

    implemented a basic UISearchDisplayController using a storyboard and a basic UITableViewController

--- UISearchDisplayController/MyTableViewController.h
+++ UISearchDisplayController/MyTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MyTableViewController.h
 //  UISearchDisplayController
 //
 //  Created by Phillip Harris on 4/19/14.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface MyTableViewController : UITableViewController
 
 @end
